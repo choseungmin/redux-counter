1ae91cb65f76346c327c6227c3c9da0ca6693e6d
seungmin <mirame@nextree.co.kr>
2018-08-13 17:55:46 +0900

    https://velopert.com/3346 진행중

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Counter from '../components/Counter';
+
+class App extends Component {
+    render() {
+        return (
+            <Counter>
+                Counter
+            </Counter>
+        );
+    }
+}
+
+export default App;
