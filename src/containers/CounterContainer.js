79aae742db0fb1d1d05fcc57cab90a76548abac5
seungmin <mirame@nextree.co.kr>
2018-08-14 16:11:50 +0900

    no message

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,11 +1,12 @@
 import Counter from '../components/Counter';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
+import { getRandomColor } from '../utils';
 
 // store 안의 state 값을 props 로 연결해줍니다.
 const mapStateToProps = (state) => ({
-    color: state.color,
-    number: state.number
+    color: state.colorData.color,
+    number: state.numberData.number
 });
 
 
@@ -18,7 +19,7 @@ const mapDispatchToProps = (dispatch) => ({
     onIncrement: () => dispatch(actions.increment()),
     onDecrement: () => dispatch(actions.decrement()),
     onSetColor: () => {
-        const color = 'black'; // 임시
+        const color = getRandomColor();
         dispatch(actions.setColor(color));
     }
 });
